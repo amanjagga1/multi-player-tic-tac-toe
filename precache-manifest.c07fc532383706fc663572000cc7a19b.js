06385b1f19e1a6622ef89c001cfabbc7b0fda898
amanjagga1 <talkinglobby@gmail.com>
2019-10-13 14:52:42 +0530

    build for github pages

---
+++ precache-manifest.c07fc532383706fc663572000cc7a19b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531465f55bd782d70327f830f5a17d3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7a9712dfd0505028044",
+    "url": "/static/css/main.49ea9c08.chunk.css"
+  },
+  {
+    "revision": "210ea4ae96b8ed58b17c",
+    "url": "/static/js/2.f48af0de.chunk.js"
+  },
+  {
+    "revision": "b7a9712dfd0505028044",
+    "url": "/static/js/main.2fe0c786.chunk.js"
+  },
+  {
+    "revision": "1afd4cff43a2b0a4d4c0",
+    "url": "/static/js/runtime-main.66daa18c.js"
+  }
+]);
